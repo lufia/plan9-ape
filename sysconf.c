03620a284bebc2650d91868335758c196f338a3f
lufia <lufia@lufia.org>
2019-03-21 20:06:40 +0900

    add sysconf(_SC_PAGESIZE)

---
+++ sysconf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct entry entry;
+struct entry {
+	int conf;
+	long v;
+} tests[] = {
+	{ _SC_PAGESIZE, 4096 },
+	{ 0, 0 },
+};
+
+int
+main(void)
+{
+	entry *tt;
+	long v;
+	int status;
+
+	status = 0;
+	for(tt = tests; tt->conf; tt++){
+		v = sysconf(tt->conf);
+		if(v != tt->v){
+			fprintf(stderr, "sysconf(%d) = %ld; want %ld\n", tt->conf, v, tt->v);
+			status = 1;
+		}
+	}
+	return status;
+}
