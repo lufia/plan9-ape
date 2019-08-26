27254ea613ba47acd9767187249a4e9ee96543c2
lufia <lufia@lufia.org>
2019-08-27 01:11:38 +0900

    update

--- vsnprintf.c
+++ vsnprintf.c
@@ -7,7 +7,7 @@ main(void)
 	int n, want;
 
 	want = 8;
-	n = snprintf(NULL, 0, "size %d\n", 10);
+	n = snprintf(buf, 0, "size %d\n", 10);
 	if(n != want)
 		fprintf(stderr, "nil: n = %d; want %d\n", n, want);
 	n = snprintf(buf, sizeof(buf), "size %d\n", 10);
